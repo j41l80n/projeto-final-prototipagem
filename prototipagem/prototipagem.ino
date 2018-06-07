febef3dbde39bfcc740b8125a50dd1b4686b0fc7
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-07 14:10:45 -0300

    iniciando codigos na funcao loop()

--- prototipagem/prototipagem.ino
+++ prototipagem/prototipagem.ino
@@ -27,5 +27,28 @@ void setup() {
 }
 
 void loop() {
-  
+  if (conteudo.substring(1) == "D8 4B 12 22")
+  {
+    microservo9g.write(-90);
+    digitalWrite(led_liberado, HIGH);
+    Serial.println("Cartao1 - Acesso liberado !");
+    Serial.println();
+    delay(3000);
+    microservo9g.write(90);
+    digitalWrite(led_liberado, LOW);
+  }
+  if (conteudo.substring(1) == "87 4B DC 8A")
+  {
+    Serial.println("Cartao2 - Acesso negado !!");
+    Serial.println();
+    // Pisca o led vermelho
+    for (int i= 1; i<5 ; i++)
+    {
+      digitalWrite(led_negado, HIGH);
+      delay(200);
+      digitalWrite(led_negado, LOW);
+      delay(200);
+    }
+  }
+  delay(1000);
 }
