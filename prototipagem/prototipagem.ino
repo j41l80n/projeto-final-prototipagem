1d7c049e65442740dc75e2162b0ae1d03e06f149
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-07 14:09:31 -0300

    incluindo bibliotecas necessarias e inicio do codigo setup

--- prototipagem/prototipagem.ino
+++ prototipagem/prototipagem.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h> 
 
+Servo microservo9g;
+
+// Definicoes pino modulo RC522
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+void setup() {
+  // define que o servo esta ligado a porta digital 3
+  microservo9g.attach(3);
+  // move o servo para a posicao inicial
+  microservo9g.write(90);
+  // inicia a serial
+  Serial.begin(9600);
+  // inicia  SPI bus
+  SPI.begin();
+  // inicia MFRC522
+  mfrc522.PCD_Init(); 
+  // Mensagens iniciais no serial monitor
+  //  Serial.println("Aproxime o seu cartao do leitor...");
+  //  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
